438714b7bd7b09244cf6f998155356fca8c0bd4d
vladimir <grun_gespenst@tut.by>
2023-06-22 23:06:24 +0300

    test ok

--- app/tests.py
+++ app/tests.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase, APIClient
+from rest_framework.test import APITestCase
 
 from .models import Post, Category
 
@@ -16,14 +16,12 @@ class CreatePostTests(APITestCase):
         """
         url = reverse('view_create')
         cat = Category.objects.create(title="test_category")
-        user = User.objects.create_user(username='test_user', password='Bb*12345678')
-        print(user, type(user))
+        user = User.objects.create_user(username='test_user',
+                                        password='Bb*12345678'
+                                        )
+        self.client.login(username='test_user', password='Bb*12345678')
 
-        clients = APIClient()
-        clients.login(username='test_user', password='Bb*12345678')
-
-
-        data = {"author": user,
+        data = {"author": user.pk,
                 "title": "Record",
                 "body": "Some sport content",
                 "views": 32,
@@ -34,3 +32,5 @@ class CreatePostTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Post.objects.count(), 1)
         self.assertEqual(Post.objects.get().title, "Record")
+        self.assertEqual(Post.objects.get().body, "Some sport content")
+        self.assertEqual(Post.objects.get().views, 32)
