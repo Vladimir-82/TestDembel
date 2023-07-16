75e77d0295eb36f94318a86d4954f62f786c9c26
vladimir <grun_gespenst@tut.by>
2023-07-16 12:53:01 +0300

    js static

--- TestDembel/urls.py
+++ TestDembel/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')), # подключение авторизации по сессии
     path('api/v1/', include('app.urls')),
-    # path('admin/app/post', get_code, name='generate'),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
